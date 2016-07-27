e2e7bf755b181f5d0f2f4a677e0b6d0607af5cf5
tinotk <tinokhong@gmail.com>
2016-07-26 23:16:21 -0700

    v1.1 update
    - Added Random offset to randomize clicking positions about original coordinate.
    - Enter your own # of clicks.

--- autoclicker.lua
+++ autoclicker.lua
@@ -1,16 +1,20 @@
---Auto Clicker v1.0
+--Auto Clicker v1.1
 --by tinotk
 --Link to Original thread: http://ankulua.boards.net/thread/115/auto-clicker
 
 setImmersiveMode(true)
 
 clicks = {}
+
 dialogInit()
-list = {"1","2","3","4","5"}
-addSpinnerIndex("clicknum", list, "1")
+--list = {"1","2","3","4","5"}
+--addSpinnerIndex("clicknum", list, "1")
+--addSpinner("clicknum",list,"1")
+addEditNumber("clicknum",5)
 dialogShow("How many click location?")
 
 dialogInit()
+clicknum=tonumber(clicknum)
 for i = 1, clicknum do
 	addTextView("Click#"..i.."    ")
 	addTextView("X:")
@@ -19,38 +23,40 @@ for i = 1, clicknum do
 	addEditNumber("ycord"..i,0)
 	addTextView("Wait:")
 	addEditNumber("wait"..i,1)
-	addCheckBox("continuous"..i,"Continuous?",false)
+	addCheckBox("continuous"..i,"Continuous ",false)
+	addCheckBox("rand"..i,"Randomize",false)
 	newRow()
 end
+addTextView("Random Offset:")
+addEditNumber("offset",0)
 dialogShow("Click settings")
 
-if(1 <= clicknum) then
-	clicks[1] = {location = Location(xcord1, ycord1), wait=wait1, rpt=continuous1}
-end
-if(2 <= clicknum) then
-	clicks[2] = {location = Location(xcord2, ycord2), wait=wait2, rpt=continuous2}
-end
-if(3 <= clicknum) then
-	clicks[3] = {location = Location(xcord3, ycord3), wait=wait3, rpt=continuous3}
-end
-if(4 <= clicknum) then	
-	clicks[4] = {location = Location(xcord4, ycord4), wait=wait4, rpt=continuous4}
-end
-if(5 <= clicknum) then
-	clicks[5] = {location = Location(xcord5, ycord5), wait=wait5, rpt=continuous5}
+for i=1,clicknum do
+	clicks[i] = {location = Location(_G["xcord"..i], _G["ycord"..i]), wait=_G["wait"..i], rpt=_G["continuous"..i], rand=_G["rand"..i]}
 end
 
 while true do
 	for i=1,#clicks do
+		if(clicks[i].rand) then
+			math.randomseed(os.time())
+			math.random() ; math.random()
+			--randomOffset = math.random(-math.floor(offset/2),offset-math.floor(offset/2))
+			--randomOffset2 = math.random(-math.floor(offset/2),offset-math.floor(offset/2))
+			randomOffset = math.random(-offset,offset)
+			randomOffset2 = math.random(-offset,offset)
+			loc = Location(clicks[i].location:getX()+randomOffset,clicks[i].location:getY()+randomOffset2)
+		else
+			loc = clicks[i].location
+		end
 		print("Click "..i.."  ".."of "..#clicks)
 		if(clicks[i].rpt) then
 			print("Continuous click at:")
-			print(clicks[i].location)
-			continueClick(clicks[i].location,100)
+			print(loc)
+			continueClick(loc,100)
 		else
 			print("Single click at:")
-			print(clicks[i].location)
-			click(clicks[i].location)
+			print(loc)
+			click(loc)
 		end
 		wait(clicks[i].wait)
 	end
